3e376f1a6fb90b6bb8df5f98a3805966e108a02e
z00295v <z00295v@target.com>
2018-05-06 14:35:15 +0530

    Basic API Structure

---
+++ src/main/java/OlaApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+public class OlaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OlaApplication.class,args);
+    }
+}
---
+++ src/main/java/controller/OlaController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OlaController {
+
+
+}
